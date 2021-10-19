41452c5efb69ece6e88c0dbcda078525a617f211
wahaha <e314520513@gmail.com>
2021-10-19 10:27:46 +0800

    update

--- controllers/default.go
+++ controllers/default.go
@@ -7,8 +7,17 @@ import (
 type MainController struct {
 	beego.Controller
 }
-
+type Human struct{
+	name string
+	age init
+	phone string
+}
+func (h Human) String() string{
+	return h.name+h.age+h.phone
+}
 func (c *MainController) Get() {
+	Bob := Human{"Anson",40,"09xx-xxx-xxx"}
+	fmt.Println("This Human is : ",Bob)
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"

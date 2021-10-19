5fe8ea21d880c87e4d429f97664a09a223fed84c
wahaha <e314520513@gmail.com>
2021-10-19 17:28:32 +0800

    update

--- controllers/default.go
+++ controllers/default.go
@@ -2,25 +2,13 @@ package controllers
 
 import (
 	beego "github.com/beego/beego/v2/server/web"
-	"fmt"
-	"strconv"
 )
 
 type MainController struct {
 	beego.Controller
 }
-type Human struct{
-	name string
-	age int
-	phone string
-}
-func (h Human) String() string{
-	return h.name+strconv.Itoa(h.age)+h.phone
-}
-func (c *MainController) Get() {
-	Bob := Human{"Anson",40,"09xx-xxx-xxx"}
-	fmt.Println("This Human is : ",Bob)
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+
+func (this *MainController) Get() {
+	this.TplName = "index.html"
+	
 }
--- routers/router.go
+++ routers/router.go
@@ -7,4 +7,6 @@ import (
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+	beego.Router("/task/",&controllers.TaskController{},"get:ListTasks;post:NewTask")
+	beego.Router("/task/:id:int", $controllers.TaskController{},"get:GetTask;put:UpdateTask")
 }
